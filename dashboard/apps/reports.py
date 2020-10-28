e45f752188094bd7c3ab82ef9823ed3adc900342
iolambda1 <i.hardwick97@gmail.com>
2020-10-28 18:03:12 +0800

    Add table of top 100 reports based on near miss events

--- dashboard/apps/reports.py
+++ dashboard/apps/reports.py
@@ -1,24 +1,35 @@
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from app import events
+from app import map_data
 
-options = [{'label': filename, 'value': filename} for filename in events.filename.unique()]
+dataframe = map_data.nlargest(100,'score')[['anumber', 'title','report_year', 'score']]
+cols = ['A Number', 'Report Title','Year','Score']
+dataframe.columns = cols
 
-layout = html.Div([
-    html.H3('reports'),  # header name
-    dcc.Dropdown(
-            id='reports-dropdown',
-            options=options,
-    ),
-    html.Div(id='reports-display-value'),  # we will output the result from our dropdown here
+def Table():
+    rows = []
+    for i in range(len(dataframe)):
+        row = []
+        for col in cols:
+            value = dataframe.iloc[i][col]
+            cell = html.Td(children=value)
+            row.append(cell)
+        rows.append(html.Tr(row))
+
+    table = [html.Thead(html.Tr([html.Th(col) for col in cols])), html.Tbody(rows)]
+    return dbc.Table(table,
+                     bordered=True,
+                     dark=True,
+                     hover=True,
+                     responsive=True,
+                     striped=True)
+
+
+layout = html.Div(children=[
+    html.H2(children='Top 100 Reports'),
+    Table()
 ])
-
-# handle the user interactivity for our dropdown
-@app.callback(
-        Output('reports-display-value', 'children'),
-        [Input('reports-dropdown', 'value')])
-def display_value(value):  # define the function that will compute the output of the dropdown
-    return f'You have selected "{value}"'
