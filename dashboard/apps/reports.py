cb29a555c61af85d908cf49b49427b201132331f
iolambda1 <i.hardwick97@gmail.com>
2020-10-28 18:20:55 +0800

    Change to DataTable

--- dashboard/apps/reports.py
+++ dashboard/apps/reports.py
@@ -1,6 +1,9 @@
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
+
+import dash_table
+
 from dash.dependencies import Input, Output
 
 from app import app
@@ -10,26 +13,18 @@ dataframe = map_data.nlargest(100,'score')[['anumber', 'title','report_year', 's
 cols = ['A Number', 'Report Title','Year','Score']
 dataframe.columns = cols
 
-def Table():
-    rows = []
-    for i in range(len(dataframe)):
-        row = []
-        for col in cols:
-            value = dataframe.iloc[i][col]
-            cell = html.Td(children=value)
-            row.append(cell)
-        rows.append(html.Tr(row))
-
-    table = [html.Thead(html.Tr([html.Th(col) for col in cols])), html.Tbody(rows)]
-    return dbc.Table(table,
-                     bordered=True,
-                     dark=True,
-                     hover=True,
-                     responsive=True,
-                     striped=True)
-
-
 layout = html.Div(children=[
     html.H2(children='Top 100 Reports'),
-    Table()
+    dash_table.DataTable(id='table',
+    columns=[{"name": i, "id": i} for i in dataframe.columns],
+    data=dataframe.to_dict('records'),
+    style_cell={'textAlign': 'left',
+    'backgroundColor': 'rgb(50, 50, 50)',
+        'color': 'white',
+        'whiteSpace': 'normal',
+        'height': 'auto',
+        'minWidth': '180px',
+        'width': '180px',
+        'maxWidth': '180px'},
+    )
 ])
