008d397f07aaf95cc5d55d9fdbff8723ceebf454
iolambda1 <i.hardwick97@gmail.com>
2020-10-29 12:10:03 +0800

    Change to sample data

--- dashboard/database.py
+++ dashboard/database.py
@@ -8,9 +8,10 @@ from pipeline.data import report_scores # function to construct report scores
 ## clearly this is not a database, but the /data folder already exists and i'd like to put functions in /pipeline, stored data in /data, and "in-memory" data shared between pages here ##
 
 # load data from file
-events = pd.read_csv(os.path.join('data','events_high-conf.csv'), index_col=0)
-events['label'] = events['label'].astype(int)
-events['prob'] = [np.random.uniform(low=0.5,high=1.0) if label else np.random.uniform(low=0.,high=0.499) for label in events['label']]
+events = pd.read_csv(os.path.join('data','event_chunks-d2v_v50-c3-e20-sample.csv'), index_col=False) #index_col=0
+events['event_text'] = [' '.join(sentences).strip() for sentences in events.event_text.values]
+events['label'] = events['pred'].astype(int)
+#events['prob'] = [np.random.uniform(low=0.5,high=1.0) if label else np.random.uniform(low=0.,high=0.499) for label in events['label']]
 
 map_data = pd.read_csv(os.path.join('data','geoview','map_data.zip'), compression='zip', index_col=0, parse_dates=['report_year','date_from','date_to'])
 map_data['report_year'] = map_data['report_year'].dt.strftime('%Y')
